4e2d095cecdd1f594b41dcb9c43c503251b168d6
Rosyid <rosyidgrobogan@gmail.com>
2021-03-30 21:07:03 +0700

    fetching data

--- src/App.js
+++ src/App.js
@@ -8,12 +8,33 @@ import AsideMenu from './components/AsideMenu';
 import Footer from './components/Footer';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  // console.log(process.env.REACT_APP_APIKEY);
+
+  React.useEffect(function () {
+    (async function () {
+      const response = await fetch(
+        'https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            accept: 'application/json',
+            'x-api-key': process.env.REACT_APP_APIKEY,
+          },
+        }
+      );
+      const { nodes } = await response.json();
+      setItems(nodes);
+      console.log('nodesnya:' + nodes);
+    })();
+  }, []);
+
   return (
     <>
       <Header />
       <Hero />
       <Browse />
-      <Arrived />
+      <Arrived items={items} />
       <Clients />
       <AsideMenu />
       <Footer />
