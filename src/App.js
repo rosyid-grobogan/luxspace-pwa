14bd51dbce15e4551886b7e3674e2b6eefca2735
Rosyid <rosyidgrobogan@gmail.com>
2021-03-30 22:55:33 +0700

    tambah carousel dan mode offline

--- src/App.js
+++ src/App.js
@@ -6,31 +6,55 @@ import Arrived from './components/Arrived';
 import Clients from './components/Clients';
 import AsideMenu from './components/AsideMenu';
 import Footer from './components/Footer';
+import Offline from './components/Offline';
 
 function App() {
   const [items, setItems] = React.useState([]);
-  // console.log(process.env.REACT_APP_APIKEY);
+  const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
 
-  React.useEffect(function () {
-    (async function () {
-      const response = await fetch(
-        'https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc',
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            accept: 'application/json',
-            'x-api-key': process.env.REACT_APP_APIKEY,
-          },
-        }
-      );
-      const { nodes } = await response.json();
-      setItems(nodes);
-      console.log('nodesnya:' + nodes);
-    })();
-  }, []);
+  function handleOfflineStatus() {
+    setOfflineStatus(!navigator.onLine);
+  }
+
+  React.useEffect(
+    function () {
+      (async function () {
+        const response = await fetch(
+          'https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc',
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              accept: 'application/json',
+              'x-api-key': process.env.REACT_APP_APIKEY,
+            },
+          }
+        );
+        const { nodes } = await response.json();
+        setItems(nodes);
+
+        // manual, setelah semua data di load dulu
+        const script = document.createElement('script');
+        script.src = '/js/carousel.js';
+        script.async = false;
+        document.body.appendChild(script);
+      })();
+
+      // didMount
+      handleOfflineStatus();
+      window.addEventListener('online', handleOfflineStatus);
+      window.addEventListener('offline', handleOfflineStatus);
+
+      return function () {
+        window.removeEventListener('online', handleOfflineStatus);
+        window.removeEventListener('offline', handleOfflineStatus);
+      };
+    },
+    [offlineStatus]
+  );
 
   return (
     <>
+      {offlineStatus && <Offline />}
       <Header />
       <Hero />
       <Browse />
---
+++ src/components/Offline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Offline() {
+  return (
+    <div className='bg-yellow-50 border-l-4 border-yellow-400 p-4'>
+      <h2>Sedang Offline ..</h2>
+    </div>
+  );
+}
+
+export default Offline;
